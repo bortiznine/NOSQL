5f511c9f486195895731d26775e8d0aaaafcc177
pengu <bortiznine@gmail.com>
2022-11-05 11:57:17 -0500

    NoSQL adjusted the following: 1. Added namespace medical_db and created tables: patient_personal_info patient_activity patient_vitals 2. Adjusted datatypes and names to values 3. Added System in for reading input for user for insert case. Will add second case for selecting and viewing data. 4. document updated.

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,52 @@ import com.datastax.driver.core.Session;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Cassandra Java Connection");
-        Cluster cluster;
-        Session session;
-        //Connect to the cluster and Keyspace ecommerce
-        cluster = Cluster.builder().addContactPoint("localhost").build();
-        session = (Session) cluster.connect("ecommerce");
-        System.out.println("Inserting Data in Cassandra");
-        session.execute("INSERT INTO products (pdt_id, cat_id, pdt_name, pdt_desc, price, shippingAddress) VALUES (001,104, 'Danby 0.7 cu. ft. Microwave Oven', 'Capacity of 0.7 cu. ft.10 different power levels', 54.00, 'Expedited')");
-        String pdtid = null, pdtname = null, pdtdesc = null;
-        float price = 0;
-        ResultSet resultset = session.execute("select * from products where pdt_id=005");
-        for (Row row : resultset) {
-            pdtid = Integer.toString(row.getInt("pdt_id"));
-            pdtname = row.getString("pdt_name");
-            pdtdesc = row.getString("pdt_desc");
-            price = row.getFloat("price");
+        System.out.println("Welcome to The Medical DB");
+        System.out.println("Are you a entering a New Patient(1), Or looking up Previous Patient(2)");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value :");
+        String str = sc.nextLine();
+        System.out.println("User input: " + str);
+        switch (Integer.parseInt(str)) {
+            case 1:
+
+            try {
+                System.out.println("Cassandra Java Connection");
+                Cluster cluster;
+                Session session;
+                //Connect to the cluster and Keyspace ecommerce
+                cluster = Cluster.builder().addContactPoint("localhost").build();
+                session = (Session) cluster.connect("medical_db");
+                String address = null, insurance = null, name = null;
+                Integer pid = null;
+                System.out.println("Inserting Data in Cassandra");
+                session.execute("INSERT INTO patient_personal_info (pid, address, insurance, name) VALUES (2,'3236 W Tom Street', 'Metlife Advantage', 'Tim')");
+
+                ResultSet resultset = session.execute("select * from patient_personal_info where pid=1");
+                for (Row row : resultset) {
+                    pid = row.getInt("pid");
+                    address = row.getString("address");
+                    insurance = row.getString("insurance");
+                    name = row.getString("name");
 
 
-            System.out.println("Product ID: " + pdtid);
-            System.out.println("Name: " + pdtname);
-            System.out.println("Description: " + pdtdesc);
-            System.out.println("Price: " + price);
+                    System.out.println("Patient ID: " + pid);
+                    System.out.println("Name: " + name);
+                    System.out.println("Address: " + address);
+                    System.out.println("Insurance: " + insurance);
 
+
+                }
+                cluster.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
 
         }
-        cluster.close();
     }
 }
